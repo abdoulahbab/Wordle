43120fd72ecb3393f4b5d6db3b3c530cfced7ed6
abdoulahbab <abdouelhabbab@gmail.com>
2024-11-23 21:44:46 +0100

    4-added-Js-base-part

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-
 const letters = document.querySelectorAll('.scoreboard-letter');
-const word="";
-console.log(letters);
-let currentIndex=0;
+let word = "";
+let currentIndex = 0;
+let count=0;
+let trials=6;
+
 function handleKeyDown(event) {
-    letter=event.key;
-    console.log(event);
-    console.log(`Key pressed: ${letter}`);
+    const key = event.key;
+    console.log(`Key pressed: ${key}`);
+   
+    handleWord(key);
+    if (trials === 0) {
+        window.alert("Sorry, you lost. Try again by reloading the page.");
+    }
+}
 
-    DisplayLetter(letter);   
+function handleWord(key) {
+    if (isLetter(key)) {
+        if(currentIndex<5){
+            letters[(6-trials)*5+currentIndex].textContent = key.toUpperCase();
+        word += key.toUpperCase();
+        currentIndex++;
+        }
+        else{
+            letters[(6-trials)*5+currentIndex-1].textContent = key.toUpperCase();
+            word = word.slice(0,-1)+key.toUpperCase();
+        }
+        
+    }
+    else if (key === 'Enter') {
+      console.log(word.length);
+      if (word.length === 5) {
+          console.log(word);
+          word = ""; 
+          currentIndex = 0;
+          trials--;
+      } else {
+          console.log("Press enter after inserting 5 letters");
+      }
+  }
+   else if (key === 'Backspace'&&currentIndex>=0) {
+    currentIndex--;
+    letters[(6 - trials) * 5 + currentIndex].textContent = "";
+    word = word.slice(0, -1);
+   }
+}
 
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
 }
 
 
 
 window.addEventListener("keydown", handleKeyDown);
-function DisplayLetter(letter){
-    if(Validate(letter)) {letters[currentIndex++].textContent = letter.toUpperCase( );
-       
-    }
-    
-
-}
-function Validate(letter){
-  return /^[a-zA-Z]$/.test(letter);
-}
